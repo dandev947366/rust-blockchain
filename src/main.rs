5b04e6cda6bf0ddcb39633b764245287b9f34119
Dan Le <dan.dev947366@gmail.com>
2024-12-06 21:12:59 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod module_4_data_structures;
+mod module_5_ownerships;
+mod module_6_struct;
+mod module_7_enum;
+mod module_8_option;
 fn main() {
     println!("\n");
-    module_4_data_structures::demo();
+    module_8_option::demo();
     println!("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
