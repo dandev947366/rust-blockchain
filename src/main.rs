4d36607c7287976cfa06b58d344a79e07134585a
Dan Le <dan.dev947366@gmail.com>
2024-12-06 21:46:11 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ mod module_5_ownerships;
 mod module_6_struct;
 mod module_7_enum;
 mod module_8_option;
+mod module_9_result;
+mod module_10_option_result;
+mod module_11_traits;
+mod module_12_concurrency;
 fn main() {
     println!("\n");
-    module_8_option::demo();
+    module_12_concurrency::demo();
     println!("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
